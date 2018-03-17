3d852904111ea26be0d4a8b951aa9dc7b04507e5
francisvalfgs <francisval20@yahoo.com.br>
2018-03-17 19:44:52 -0300

    corrigido bug nos segundos e incluido alguns comentarios

--- PIOQuailTime/src/main.cpp
+++ PIOQuailTime/src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-
-//
 #include <TimerOne.h>
 #include <SevSeg.h>
 #include <Wire.h>
@@ -8,37 +6,40 @@
 
 SevSeg sevseg; //Instantiate a seven segment controller object
 
+//protótipos
 void timerIsr();
-void menuIHM();
+void trataBotoes();
 
-const unsigned long intervalButton = 500; //tempo do botão ms
-const unsigned long intervalMenu = 3000;  //tempo de retorno do menu ms
-const unsigned long intervalBuzer = 3000; //tempo do Buzer ms
-
+// pinos de IO
 const int pinTeclado = A0;
 const int pinBuzer = A1;
 const int pinLamp = A2;
 
+// pinos do display
 byte digitPins[] = {2, 3, 4, 5};
 byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
 
+// costantes diversas
 const int minutosMax = 20;
 const int segundosMax = 59;
+const unsigned long intervalButton = 500; //tempo do botão ms
+const unsigned long intervalMenu = 3000;  //tempo de retorno do menu ms
+const unsigned long intervalBuzer = 2000; //tempo do Buzer ms
 
+//Variaveis
 struct ObjectEeprom
 {
-  byte Sminutos;
-  byte Ssegundos;
+  int Sminutos;
+  int Ssegundos;
 };
 
 ObjectEeprom varEeprom;
 
 int eeAddress = 0;
 
-volatile byte minutos;
-volatile byte segundos;
+volatile int minutos;
+volatile int segundos;
 
-byte prevMinutos;
 byte prevSegundos;
 
 volatile byte flagContando = 0; // 0 aguardando,1 aviso de final de contagem, 2 contando
@@ -68,7 +69,15 @@ void setup()
 
   byte numDigits = 4;
   bool resistorsOnSegments = true;    // resistors are on seguiments pins
-  byte hardwareConfig = COMMON_ANODE; // See README.md for options
+
+// COMMON_CATHODE 0
+// COMMON_ANODE 1
+// N_TRANSISTORS 2
+// P_TRANSISTORS 3
+// NP_COMMON_CATHODE 1
+// NP_COMMON_ANODE 0
+  byte hardwareConfig = NP_COMMON_CATHODE; // See README.md for options COMMON_ANODE
+
   bool updateWithDelays = false;      // Default. Recommended
   bool leadingZeros = false;          // Use 'true' if you'd like to keep the leading zeros
 
@@ -77,7 +86,7 @@ void setup()
 
   Serial.begin(57600);
 
-  // Initialize the digital pin as an output.
+  // Initialize the digital pin
 
   pinMode(pinTeclado, INPUT);
   pinMode(pinBuzer, OUTPUT);
@@ -90,27 +99,26 @@ void setup()
 
 void loop()
 {
-  if ((segundos != prevSegundos) || (minutos != prevMinutos))
+  if (segundos != prevSegundos) //atualiza display a cada segundo
   {
     Serial.print(minutos);
     Serial.print(":");
     Serial.println(segundos);
     prevSegundos = segundos;
-    prevMinutos = minutos;
-    sevseg.setNumber(minutos * 100 + segundos);
+    sevseg.setNumber(minutos * 100 + segundos,2);
   }
 
-  sevseg.refreshDisplay();
+  sevseg.refreshDisplay(); //refress display
 
-  if (flagContando != prevflagContando)
+  if (flagContando != prevflagContando) //verifica se houve mudança de estatus do sistema
   {
     prevflagContando = flagContando;
 
-    if (flagContando == 1)
+    if (flagContando == 1)  // 1 aviso de final de contagem
     {
-      digitalWrite(pinBuzer, HIGH);
-      digitalWrite(pinLamp, HIGH);
-      previousMillisBuzer = millis();
+      digitalWrite(pinBuzer, HIGH); // Ativa Buzer
+      digitalWrite(pinLamp, HIGH);  // Ativa lâmpada
+      previousMillisBuzer = millis(); // inicia contagem de tempo para desligar buzer
     }
   }
 
@@ -118,10 +126,11 @@ void loop()
   if ((currentMillisBuzer - previousMillisBuzer >= intervalBuzer) && (flagContando == 1))
   {
     previousMillisBuzer = currentMillisBuzer;
-    digitalWrite(pinBuzer, LOW);
+    digitalWrite(pinBuzer, LOW);  //desliga buzer
     flagContando = 0; // 0 aguardando,1 aviso de final de contagem, 2 contando
   }
-  menuIHM();
+
+  trataBotoes();
 }
 
 /// --------------------------
@@ -146,34 +155,39 @@ void timerIsr()
 
 //*****************************************************************
 //menu de controle e botões
-void menuIHM()
+void trataBotoes()
 {
+  //ler entrada AD das teclas
   int ADCteclado = analogRead(pinTeclado);
   byte buttonState;
-
+  //associa o valor da conversão AD a tecla pressionada 
   if (ADCteclado < 60)
     buttonState = 1; // tecla pressionada iniciar contagem
   else if (ADCteclado < 500)
-    buttonState = 2; // tecla pressionada menu
+    buttonState = 2; // tecla pressionada programamar
   else if (ADCteclado < 900)
     buttonState = 3; // tecla pressionada ajuste
   else
     buttonState = 0; // nenhuma tecla pressionada
 
+  //sai do menu caso nehuma tecla seja pressionada no tempo intervalMenu
   unsigned long currentMillisMenu = millis();
   if (currentMillisMenu - previousMillisMenu >= intervalMenu)
   {
     previousMillisMenu = currentMillisMenu;
     menuState = 0; // 0 funcionamento normal, 1 ajuste minutos, 2 ajuste segundos
   }
-  else if ((buttonState == 2) || (buttonState == 3))
+  
+  //Reseta o tempo do menu caso as teclas de programação sejam pressionadas
+  if ((buttonState == 2) || (buttonState == 3))  
   {
     previousMillisMenu = currentMillisMenu;
   }
 
-  if (buttonState != lastButtonState)
+//trata botões 
+  if (buttonState != lastButtonState) // se houve mudança de estado dos botões
   {
-    if (buttonState == 1)
+    if (buttonState == 1) //se foi pressionado o botão de iniciar contagem
     {
       segundos = varEeprom.Ssegundos;
       minutos = varEeprom.Sminutos;
@@ -181,21 +195,22 @@ void menuIHM()
       Timer1.start();
       digitalWrite(pinLamp, LOW);
     }
-    else if (buttonState == 2)
+    else if (buttonState == 2) //se foi pressionado o botão de programamar
     {
       segundos = varEeprom.Ssegundos;
       minutos = varEeprom.Sminutos;
-      flagContando = 1; // 0 aguardando,1 aviso de final de contagem, 2 contando
+      flagContando = 0; // 0 aguardando,1 aviso de final de contagem, 2 contando
       Timer1.stop();
       menuState++;
       if (menuState > 2)
       {
         menuState = 0;
-        EEPROM.put(eeAddress, varEeprom);
+        EEPROM.put(eeAddress, varEeprom); 
       }
     }
   }
 
+//trata botão de ajuste de minutos e segundos
   unsigned long currentMillisButton = millis();
   if ((currentMillisButton - previousMillisButton >= intervalButton) || (buttonState != lastButtonState))
   {
